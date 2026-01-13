72ba877bb7847c954b1c6e7ba3138cf839c1e9af
HiighQ42 <nurhamad@hotmail.com>
2026-01-13 11:27:31 +0100

    ft_itoa.c erstellen

---
+++ Level 4/ft_itoa/ft_itoa.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+static int	ft_len(int n)
+{
+	int	len;
+
+	len = 1;
+	if (n < 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n >= 10)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nbr)
+{
+	char	*str;
+	int		len;
+	long	n;
+
+	n = nbr;
+	len = ft_len(n);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	while (len-- > 0 && str[len] != '-')
+	{
+		str[len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
